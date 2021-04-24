f32e9a17254d3342d0f958deb9bdbe77879331ef
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-04-24 14:05:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-//   fetch('https://anapioficeandfire.com/api/books')
-//   .then(renderBooks(response) {
-//     console.log(response);
-//    return response.json();
-//  }).then(renderBooks(json) {
-//  console.log(json);
-// });
-fetch('https://anapioficeandfire.com/api/books')
-  .then(resp => resp.json())
-  .then(json => console.log(json));
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(function(response) {
+   return response.json();
+ })
+ .then(function(json) {
+   renderBooks(json);
+})
+ }
 
-}
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
