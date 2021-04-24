4fc72124cbbe86bea5a63b13015008e956ad0bfa
Israe Moubayed <israetarek@yahoo.com>
2021-04-24 08:35:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  fetch(https://anapioficeandfire.com/api/books)
-  .then function(response) {
-    return response.json()
-  }
-}
-
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(data => renderBooks(json));
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
